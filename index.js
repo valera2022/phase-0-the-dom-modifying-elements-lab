f5c7b1685f79f7b033fc32ff084c40c3ca3b3621
GeralmyUrena <geralmyuri11@gmail.com>
2022-12-06 20:08:26 -0500

    commit tree 464b7d5ac7c9b7f78a533d61e20b066fdf876b65

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+//element = 
+document.querySelector("main#main").remove()
+
+//document.createElement("h1").append("body")
+const newHeader = document.createElement("h1")
+//element.remove()
+ newHeader.setAttribute("id", "victory")
+ newHeader.textContent = "Jose is the champion"
